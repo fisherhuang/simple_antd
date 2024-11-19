cf0d85af4cecf8987de39fd17101108539bfdb3b
fisher.huang <317966647@qq.com>
2024-11-19 14:43:21 +0800

    Updates

---
+++ assets/index.stories-CNCJJgpu.js
@@ -0,0 +1,7 @@
+import{f as r}from"./index-C_FWhylE.js";import{R as a}from"./index-uubelm5h.js";const p=()=>a.createElement(a.Fragment,null,"Form Group");p.__docgenInfo={description:"",methods:[],displayName:"FormGroupStory"};const i={title:"SimpleAntd/FormGroup",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:r(),onLogout:r(),onCreateAccount:r()}},e={args:{user:{name:"Jane Doe"}}},o={};var s,t,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: "Jane Doe"
+    }
+  }
+}`,...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var c,m,u;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(u=(m=o.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};const l=["LoggedIn","LoggedOut"];export{e as LoggedIn,o as LoggedOut,l as __namedExportsOrder,i as default};
